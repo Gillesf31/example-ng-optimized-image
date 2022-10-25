50f25c84db7a329e20c85a579d2c4db9e488096f
Gilles Ferrand <gilles.ferrand@ittingbox.com>
2022-10-25 09:30:15 +0200

    Initial commit

---
+++ apps/tech-pres-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tech-pres-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'tech-pres-app';
+}
